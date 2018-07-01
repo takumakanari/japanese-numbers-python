386abd258dee0df6cb826311d6857ffc0ef47852
takumakanari <chemtrails.t@gmail.com>
2018-07-01 16:23:52 +0900

    Bump up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'japanese-numbers-python',
-  version = '0.1.1',
+  version = '0.2.0',
   packages = find_packages(
     '.',
     exclude = [
